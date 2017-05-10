0dae5ea1f2e7e276c520326ce90fc7e158218226
chloepelletier <chloe.pelletier@hei.fr>
2017-05-10 21:27:33 +0200

    v9

--- src/main/java/projet/cadre/dao/DataSourceProvider.java
+++ src/main/java/projet/cadre/dao/DataSourceProvider.java
@@ -1,21 +1,51 @@
 package projet.cadre.dao;
 
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class DataSourceProvider {
+	private static class DataSourceProviderHolder {
+		private final static DataSourceProvider instance = new DataSourceProvider();
+	}
+	
+	public static DataSourceProvider getInstance() {
+		return DataSourceProviderHolder.instance;
+	}
+
 	private static MysqlDataSource dataSource;
 
-	public static DataSource getDataSource() {
-		if (dataSource == null) {
+	private DataSourceProvider() {
+		initDataSource();
+	}
+
+	public static DataSource initDataSource() {
+		Properties jdbcProperties = new Properties();
+		InputStream configFileStream = getClass().getClassLoader().getResourceAsStream("jdbc.properties");
+		try {
+			jdbcProperties.load(configFileStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
 			dataSource = new MysqlDataSource();
 			dataSource.setServerName("vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com");
 			dataSource.setPort(3306);
 			dataSource.setDatabaseName("ohwx147hd9g2vgz4");
 			dataSource.setUser("ifw3s4zebh0lo8js");
 			dataSource.setPassword("bkc7x46u5gzdykhs");
-		}
+		
+		
+	}
+
+	public DataSource getDataSource() {
 		return dataSource;
 	}
 }
+
